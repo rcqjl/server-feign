8d3a59eb5f1cb078453395c8a5c4571b6ca483c7
rcqjl <rc_qjl@163.com>
2018-10-30 17:10:03 +0800

    feign init

---
+++ src/main/java/com/inspur/springcloud/feign/ServerFeignApplication.java
@@ -0,0 +1,16 @@
+package com.inspur.springcloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServerFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFeignApplication.class, args);
+	}
+}
---
+++ src/main/java/service/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by qujianlong on 2018/10/30.
+ */
+
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
